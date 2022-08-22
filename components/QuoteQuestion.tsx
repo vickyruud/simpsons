4b109b91517a1758617df4f99d8cea4cf07c2f44
vickyruud <aprajit.k@gmail.com>
2022-08-22 11:05:57 -0400

    add game logic and check if response is correct

--- components/QuoteQuestion.tsx
+++ components/QuoteQuestion.tsx
@@ -1,11 +1,37 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const QuoteQuestion = ({ quoteObject }: { quoteObject: any }) => {
+  const [answered, setAnswered] = useState(false);
+  const [userResponse, setUserResponse] = useState("");
+  const [wrong, setWrong] = useState(false);
+
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+    setAnswered(false);
+
+    const correct = quoteObject.character;
+    const lowerCase = correct.toLowerCase();
+    if (lowerCase.includes(userResponse)) {
+      setAnswered(true);
+      setWrong(false);
+    } else {
+      setAnswered(false);
+      setWrong(true);
+    }
+  };
+
   return (
     <div>
+      <h1>Who said this?</h1>
       <h2>{quoteObject.quote}</h2>
-      <Image src={quoteObject.image} width="400" height="400" />
+      {answered ? <p>You are Correct!!</p> : null}
+      {answered ? (
+        <Image src={quoteObject.image} width="400" height="400" />
+      ) : null}
+      {wrong ? <p>You are Wrong!</p> : null}
+      <input onChange={(e) => setUserResponse(e.target.value)}></input>
+      <button onClick={onSubmit}>Submit</button>
     </div>
   );
 };
