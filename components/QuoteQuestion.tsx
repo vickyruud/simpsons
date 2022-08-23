dc37995d17edd4ff2ef75f47dbceabc8a4da6297
vickyruud <aprajit.k@gmail.com>
2022-08-22 23:18:36 -0400

    add buttons, cycle through the quotes and reload quotes at end

--- components/QuoteQuestion.tsx
+++ components/QuoteQuestion.tsx
@@ -1,10 +1,33 @@
 import Image from "next/image";
 import React, { useState } from "react";
+import styled from "styled-components";
 
-const QuoteQuestion = ({ quoteObject }: { quoteObject: any }) => {
-  const [answered, setAnswered] = useState(false);
+const QuoteQuestion = ({
+  quoteObject,
+  fetchQuotes,
+  setAnswered,
+  setWrong,
+  setCount,
+  handleNext,
+  answered,
+  wrong,
+  count,
+  errorMessage,
+  setErrorMessage,
+}: {
+  quoteObject: any;
+  fetchQuotes: any;
+  setAnswered: any;
+  setWrong: any;
+  setCount: any;
+  handleNext: any;
+  answered: boolean;
+  wrong: boolean;
+  count: number;
+  errorMessage: string;
+  setErrorMessage: any;
+}) => {
   const [userResponse, setUserResponse] = useState("");
-  const [wrong, setWrong] = useState(false);
 
   const onSubmit = (event: any) => {
     event.preventDefault();
@@ -12,30 +35,47 @@ const QuoteQuestion = ({ quoteObject }: { quoteObject: any }) => {
 
     const correct = quoteObject.character;
     const lowerCase = correct.toLowerCase();
-    if (lowerCase.includes(userResponse)) {
-      setAnswered(true);
-      setWrong(false);
+    if (userResponse.trim()) {
+      if (lowerCase.includes(userResponse.toLowerCase())) {
+        setAnswered(true);
+        setWrong(false);
+        setErrorMessage("");
+      } else {
+        setAnswered(false);
+        setWrong(true);
+        setErrorMessage("");
+      }
     } else {
-      setAnswered(false);
-      setWrong(true);
+      setErrorMessage("Answer Should not be blank");
     }
   };
 
   return (
     <div>
       <h1>Who said this?</h1>
-      <h2>{quoteObject.quote}</h2>
+      <h2>{count <= 9 ? quoteObject.quote : null} </h2>
       {answered ? <p>You are Correct!!</p> : null}
       {answered ? (
         <Image src={quoteObject.image} width="400" height="400" />
       ) : null}
       {wrong ? <p>You are Wrong!</p> : null}
+      {errorMessage ? <p>{errorMessage}</p> : null}
       <form onSubmit={onSubmit}>
         <input onChange={(e) => setUserResponse(e.target.value)}></input>
-        <button type="submit">Submit</button>
+        <StyledButton type="submit">Submit</StyledButton>
+        <StyledButton onClick={handleNext}>Next</StyledButton>
       </form>
     </div>
   );
 };
 
 export default QuoteQuestion;
+
+const StyledButton = styled.button`
+  background: #156450;
+  color: white;
+  border-radius: 3px;
+  border: 2px solid blue;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
--- pages/game.tsx
+++ pages/game.tsx
@@ -5,13 +5,34 @@ import styled from "styled-components";
 
 const Game = () => {
   const [quotes, setQuotes] = useState([]);
+  const [answered, setAnswered] = useState(false);
+
+  const [wrong, setWrong] = useState(false);
   const [count, setCount] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const fetchQuotes = () => {
     axios
-      .get("https://thesimpsonsquoteapi.glitch.me/quotes?count=10")
+      .get(`https://thesimpsonsquoteapi.glitch.me/quotes?count=10`)
       .then((res) => setQuotes(res.data))
       .catch((e) => console.log(e.message));
+  };
+
+  const handleNext = (event: any) => {
+    event.preventDefault();
+    console.log("here");
+    if (count < 9) {
+      setCount((prevCount) => prevCount + 1);
+      setAnswered(false);
+      setWrong(false);
+      setErrorMessage("");
+    } else {
+      setErrorMessage("");
+      fetchQuotes();
+      setAnswered(false);
+      setWrong(false);
+      setCount(0);
+    }
   };
 
   useEffect(() => {
@@ -20,7 +41,21 @@ const Game = () => {
 
   return (
     <Container>
-      {quotes.length > 1 ? <QuoteQuestion quoteObject={quotes[count]} /> : null}
+      {quotes.length > 1 ? (
+        <QuoteQuestion
+          quoteObject={quotes[count]}
+          fetchQuotes={fetchQuotes}
+          answered={answered}
+          setAnswered={setAnswered}
+          wrong={wrong}
+          setWrong={setWrong}
+          handleNext={handleNext}
+          setCount={setCount}
+          count={count}
+          errorMessage={errorMessage}
+          setErrorMessage={setErrorMessage}
+        />
+      ) : null}
     </Container>
   );
 };
@@ -29,5 +64,4 @@ export default Game;
 
 const Container = styled.div`
   padding: 10px;
-  
 `;
